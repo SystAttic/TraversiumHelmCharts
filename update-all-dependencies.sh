cc120b271d93a75fee540b7859b2bfefbcf42aab
Maja Razinger <maja.razinger@better.care>
2026-01-10 21:12:26 +0100

    Add helm charts for report service

--- update-all-dependencies.sh
+++ update-all-dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Update Helm dependencies for all services
-services=("file-storage-service" "notification-service" "trip-service" "audit-service" "user-service" "social-service" "moderation-service" "keycloak" "tenant-service" "gateway-service")
+services=("file-storage-service" "notification-service" "trip-service" "audit-service" "user-service" "social-service" "moderation-service" "keycloak" "tenant-service" "reporting-service")
 
 echo "Updating Helm dependencies for all services..."
 
